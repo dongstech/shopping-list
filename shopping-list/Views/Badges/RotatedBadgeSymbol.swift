0280295c22a562c17a3770d42c9d80308710abe0
Fuqiang Dong <fqdong@MacBookPro.attlocal.net>
2025-04-08 01:32:31 -0500

    feat: completing Drawing Paths and Shapes

---
+++ shopping-list/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  shopping-list
+//
+//  Created by Fuqiang Dong on 4/8/25.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+       BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+        
+}
